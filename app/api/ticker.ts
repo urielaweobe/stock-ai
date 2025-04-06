75996db6e475c082955e9b3531429a24d627a163
urielaweobe <urielaweobe@gmail.com>
2025-04-06 20:03:18 +0100

    refactor: update API endpoints to use environment variables and remove unused assets

--- app/api/ticker.ts
+++ app/api/ticker.ts
@@ -3,7 +3,7 @@ import { dates } from "~/utils/dates";
 
 export const fetchTickerList = async () =>
   await fetch(
-    `https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit=1000&sort=locale&locale=global&apiKey=${
+    `${import.meta.env.VITE_POLYGON_BASE_API}/v3/reference/tickers?market=stocks&active=true&order=asc&limit=1000&sort=locale&locale=global&apiKey=${
       import.meta.env.VITE_POLYGON_API
     }`,
     {
@@ -16,7 +16,7 @@ export const fetchTickerList = async () =>
 
 export const fetchStockData = async (ticker: string) =>
   await fetch(
-    `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${
+    `${import.meta.env.VITE_POLYGON_BASE_API}/v2/aggs/ticker/${ticker}/range/1/day/${
       dates.startDate
     }/${dates.endDate}?apiKey=${import.meta.env.VITE_POLYGON_API}`,
     {
