415ff76693dc8fb31181368fda2430c292d3306a
urielaweobe <urielaweobe@gmail.com>
2025-08-02 17:22:30 +0100

    feat: implement stock data fetching and processing in worker and eodhd worker

--- app/api/ticker.ts
+++ app/api/ticker.ts
@@ -1,3 +1,4 @@
+import type { FinancialData } from "./../utils/interface";
 // Note to self: The EOD API is not free. You need to sign up for an account and get an API key.
 // The free tier allows you to make 20 requests per day. You can upgrade to a paid plan for more requests.
 // The API key is stored in the .env file as VITE_EOD_API. You can use this key to make requests to the API.
@@ -13,12 +14,40 @@ interface FetchTickerData {
   endDate: string;
 }
 
-export const fetchTickerData = async ({ code, exchange, startDate, endDate }: FetchTickerData) =>
+export const fetchTickerData = async ({
+  code,
+  exchange,
+  startDate,
+  endDate,
+}: FetchTickerData) =>
   await fetch(
-    `https://eodhd.com/api/eod/${code}.${exchange}?from=${startDate}&to=${endDate}&period=d&api_token=${
-      import.meta.env.VITE_EOD_API
-    }&fmt=json`,
+    `https://eodhd-api-worker.urielaweobe.workers.dev/?code=${code}&exchange=${exchange}&startDate=${startDate}&endDate=${endDate}`,
     {
       method: "GET",
     }
   );
+
+const url = "https://stock-ai-worker.urielaweobe.workers.dev";
+
+export const fetchReport = async ({
+  data,
+  code,
+  ticker,
+  currency,
+  startDate,
+  endDate,
+}: FinancialData) =>
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      data,
+      code,
+      ticker,
+      currency,
+      startDate,
+      endDate,
+    }),
+  });
